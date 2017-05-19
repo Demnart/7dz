3397432d89854ca4b679b01a85fac1a9f07421ef
genjo <insidia93@gmail.com>
2017-05-19 14:36:55 +0300

    Стандартная форма загрузки файлов на сайт

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,6 +8,16 @@
     <title>Главная</title>
 </head>
 <body>
-
+<?php
+if (isset($_SESSION['error'])){
+    echo $_SESSION['error'];
+    unset($_SESSION['error']);
+}
+?>
+<form action="/handler.php" method="post" enctype="multipart/form-data">
+    <input type="text" name="name"><br>
+    <input type="file" name="image">
+    <input type="submit" value="Отправить">
+</form>
 </body>
 </html>
